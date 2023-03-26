3c4d104dd419d61f1a4099908978490daaf458ba
Ed <lookatemail@gmail.com>
2023-03-26 11:50:10 +0100

    Add notes to snags

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,50 @@
-'use client' // ⚠️ This is a client-side only component
+'use client'; // ⚠️ This is a client-side only component
 
-import './page.css'
-import ProfilePic from './assets/images/ProfilePic.png'
-import slider1 from './assets/images/slider1.png'
-import slider2 from './assets/images/slider2.png'
-import slider3 from './assets/images/slider3.png'
+import './page.css';
+import ProfilePic from './assets/images/ProfilePic.png';
+import slider1 from './assets/images/slider1.png';
+import slider2 from './assets/images/slider2.png';
+import slider3 from './assets/images/slider3.png';
 
 // --------------------------------------------------------------------------------
 // 📌  Component Imports
 // --------------------------------------------------------------------------------
-import { Badge } from './components/Badge'
-import { Card } from './components/Card'
-import Reserve from './components/Reserve'
-import myImage from '../app/assets/images/Rectangle 22766.png'
-import Header from './components/Header'
-import { useState } from 'react'
-import { SearchResult } from './components/Header/headerInterface'
-import CardSlider from './components/CardSlider'
-import { SlideImageInterface } from './components/CardSlider/interface'
-import BarChart from './components/BarChart'
-import PieChart from './components/PieChart'
+import { Badge } from './components/Badge';
+import { Card } from './components/Card';
+import Reserve from './components/Reserve';
+import myImage from '../app/assets/images/Rectangle 22766.png';
+import Header from './components/Header';
+import { useState } from 'react';
+import { SearchResult } from './components/Header/headerInterface';
+import CardSlider from './components/CardSlider';
+import { SlideImageInterface } from './components/CardSlider/interface';
+import BarChart from './components/BarChart';
+import PieChart from './components/PieChart';
 
 export default function Home() {
   // --------------------------------------------------------------------------------
   // 📌  Component List Page
   // This page is client-side only, so we can use React Hooks.
   // --------------------------------------------------------------------------------
-  const [fromDate, setFromDate] = useState<string>('11/15/2022')
-  const [tillDate, setTillDate] = useState<string>('11/15/2022')
+  const [fromDate, setFromDate] = useState<string>('11/15/2022');
+  const [tillDate, setTillDate] = useState<string>('11/15/2022');
   const handleReserveData = (fromDate: string, tillDate: string) => {
     // handle reserve data here
-    console.log('From', fromDate, 'Till', tillDate)
-  }
+    console.log('From', fromDate, 'Till', tillDate);
+  };
 
   const navList = [
     { slug: '/home', title: 'Home' },
     { slug: '/browse', title: 'Browse' },
     { slug: '/listItem', title: 'List an Item' },
-  ]
+  ];
 
   const [searchResult, setSearchResult] = useState<SearchResult>({
     searchValue: '',
     optionValue: '',
-  })
+  });
 
-  console.log('SEARCH RESULT', searchResult)
+  console.log('SEARCH RESULT', searchResult);
 
   // slider images for prop
 
@@ -73,10 +73,10 @@ export default function Home() {
       description:
         'Sed ut perspiciatis unde omnis iste natus error sit voluptatem usantium dolor ed ut perspiciatis unde omnis iste',
     },
-  ]
+  ];
 
   // Bar Chart Data
-  const [activeTab, setActiveTab] = useState(1)
+  const [activeTab, setActiveTab] = useState(1);
   const barChartData: string[] = [
     '$12,000',
     '$15,000',
@@ -90,7 +90,7 @@ export default function Home() {
     '$21,000',
     '$19,000',
     '$10,000',
-  ]
+  ];
 
   const barLabels: string[] = [
     'Jan',
@@ -105,7 +105,7 @@ export default function Home() {
     'Oct',
     'Nov',
     'Dec',
-  ]
+  ];
 
   // Pie chart Data
   const data = {
@@ -119,29 +119,40 @@ export default function Home() {
         hoverOffset: 4,
       },
     ],
-  }
+  };
 
   return (
-    <div className='container-wrapper'>
-      <div className='container'>
-        <Badge value='Com #1' backgroundColor='#c51111' />
+    <div className="container-wrapper">
+      <div className="container">
+        <Badge value="Com #1" backgroundColor="#c51111" />
+        <Badge value="Com #2" />
+        <Badge value="Com #4" backgroundColor="#d2d2d2" />
+
+        <div
+          style={{
+            padding: '24px 0',
+            color: '#222',
+            textAlign: 'center',
+            userSelect: 'none',
+          }}
+        >
+          <h1>Developed Components</h1>
+        </div>
         <Header navList={navList} setSearchResult={setSearchResult}>
           Rentables
         </Header>
-        <Badge value='Com #2' />
         <Card
           imageSrc={myImage}
-          cardPrice='$23'
-          PriceDividedBy='Day'
-          distance='1Km away'
+          cardPrice="$23"
+          PriceDividedBy="Day"
+          distance="1Km away"
           rating={4.9}
           ratingCount={23}
         >
-          Best Dslr canon 100d camera
+          Best Dslr canon 100d camera + Irure aliquip nostrud magna sint veniam
+          ipsum tempor laboris officia.
         </Card>
-        <Badge value='Com #3' backgroundColor='#d2d2d2' />
         <CardSlider ratingCount={4.9} slideImages={slideImageList}></CardSlider>
-        <Badge value='Com #4' backgroundColor='#d2d2d2' />
         <BarChart
           barChartData={barChartData}
           activeTab={activeTab}
@@ -149,7 +160,6 @@ export default function Home() {
           barLabels={barLabels}
         ></BarChart>
         <PieChart data={data}></PieChart>
-        <Badge value='Com #5' backgroundColor='#d2d2d2' />
         <Reserve
           reservePrice={'$23'}
           reserveDay={'day'}
@@ -166,5 +176,5 @@ export default function Home() {
         />
       </div>
     </div>
-  )
+  );
 }
